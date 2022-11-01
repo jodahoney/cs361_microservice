22c27421d607139bfec70e19aa83fbbc1ea471ae
Joseph Dehoney <josephdehoney@gmail.com>
2022-10-31 22:49:14 -0700

    added UML screenshot

--- api.py
+++ api.py
@@ -13,28 +13,6 @@ def square_results(num):
     return jsonify(response)
 
 
-@dataclass
-class Macronutrient:
-    name:str
-    cals_per_gram: int
-
-    def calc_calories(self) -> int:
-        if self.name in ["carbohydrate", "protein"]:
-            return self.cals_per_gram * 4
-        else:
-            return self.cals_per_gram * 9
-
-@app.route('/calculate-cals/<string:name>/<int:grams>')
-def calculate_cals(name, grams):
-    if name and grams:
-        macro = Macronutrient(name, grams)
-        response = {
-            "macro-name": name,
-            "grams": grams,
-            "calories": macro.calc_calories()
-        }
-        return jsonify(response)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
